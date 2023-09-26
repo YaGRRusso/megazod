d091dd551a291a9c55159afc1820546cb601f186
Yago Russo <yagrrusso@gmail.com>
2023-09-26 16:42:56 -0300

    feat: check input helper

---
+++ src/helpers/check.helper.ts
@@ -0,0 +1,12 @@
+const check = {
+  required: (data: string) => !data && 'required',
+  email: (data: string) =>
+    !/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(data) && 'invalidEmail',
+  number: (data: string) => !/([0-9])/.test(data) && 'needNumber',
+  capitalLetter: (data: string) =>
+    !/([a-z].*[A-Z])|([A-Z].*[a-z])/.test(data) && 'needCapitalLetter',
+  specialCharacter: (data: string) =>
+    !/([!,%,&,@,#,$,^,*,?,_,.,;,~,+,-])/.test(data) && 'needSpecialCharacter',
+}
+
+export default check
