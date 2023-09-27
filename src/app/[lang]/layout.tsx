86f9bf23d27e553de75b9d8d475c0a52955f837c
Yago Russo <yagrrusso@gmail.com>
2023-09-27 13:05:40 -0300

    feat: home page

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -22,12 +22,12 @@ export default function RootLayout({
       <body
         className={clsx(inter.variable, 'bg-dark font-inter text-gray-100')}
       >
-        <main className="container flex h-full min-h-screen">
+        <main className="container flex h-full min-h-screen gap-16">
           <div className="flex max-w-xs flex-[1] items-center justify-end max-lg:hidden">
             <PageSidebar />
           </div>
           <div className="flex flex-[2] items-center justify-center">
-            <div className="flex flex-col gap-16">{children}</div>
+            <div className="flex max-w-lg flex-col gap-16">{children}</div>
           </div>
         </main>
       </body>
--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,5 +1,15 @@
 import Image from 'next/image'
 
 export default function Home() {
-  return <Image src="/megazod.png" alt="logo" width={300} height={300} />
+  return (
+    <div className="flex flex-col items-center justify-center gap-4 text-center">
+      <Image src="/megazod.png" alt="logo" width={420} height={420} />
+      <h1 className="text-3xl font-bold text-primary">Megazod</h1>
+      <p>
+        Talk sobre abordagens de formulários em React/Next tratando erros de
+        digitação, formatação e verificação de dados no lado do client no pré
+        envio de formulários
+      </p>
+    </div>
+  )
 }
