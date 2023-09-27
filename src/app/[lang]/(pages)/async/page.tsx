3812715aafd8bc36315561d3415060946dc18f81
Yago Russo <yagrrusso@gmail.com>
2023-09-27 18:00:26 -0300

    feat: empty pages

--- src/app/[lang]/(pages)/async/page.tsx
+++ src/app/[lang]/(pages)/async/page.tsx
@@ -4,12 +4,6 @@ export default function Page() {
   return (
     <div className="flex flex-col items-center justify-center gap-4 text-center">
       <Image src="/megazod.png" alt="logo" width={420} height={420} />
-      <h1 className="text-3xl font-bold text-primary">Megazod</h1>
-      <p>
-        Talk sobre abordagens de formulários em React/Next tratando erros de
-        digitação, formatação e verificação de dados no lado do client no pré
-        envio de formulários
-      </p>
     </div>
   )
 }
